9347b3e21627e100d3f2ab37c85efe4dec3461f3
iamalmiir <iamalmiir@gmail.com>
2022-02-14 06:54:29 -0500

    first commit

---
+++ myapp.py
@@ -0,0 +1,47 @@
+from tkinter import Label, Entry, Button, StringVar
+import yt_dlp
+
+BG_COLOR = "#171717"
+FONT_TITLE = ("Arial", 30)
+
+
+class App:
+    def __init__(self, root) -> None:
+        self.root = root
+        self.url_variable = StringVar()
+
+        # Styling YT
+        Label(text="YT ", bg=BG_COLOR, foreground="red", font=FONT_TITLE).grid(row=0, column=1, columnspan=2)
+        Label(
+            text=" Download ⬇",
+            background=BG_COLOR,
+            foreground="#fff",
+            font=FONT_TITLE,
+        ).grid(row=0, column=2)
+
+        # URL Input Label
+        # url_label = Label(
+        #     text="Enter URL: ", foreground="#fff", bg=BG_COLOR, font=("Arial", 25)
+        # )
+        # url_label.place(relx=0.3, rely=0.3)
+
+        # URL Input
+        # url_input = Entry(
+        #     textvariable=self.url_variable,
+        #     font=("Arial", 20, "normal"),
+        #     background="#fff",
+        #     foreground="#000",
+        # )
+        # url_input.place(relx=0.45, rely=0.3)
+
+        def submit() -> None:
+            with yt_dlp.YoutubeDL() as ydl:
+                ydl.download([self.url_variable.get()])
+
+            # success_button = Button(text="Video has been downloaded successfully.")
+            # success_button.place(relx=0.6, rely=0.6)
+
+        # submit_button = Button(
+        #     text="Submit", background="#fff", foreground="#000", command=submit
+        # )
+        # submit_button.place(relx=0.5, rely=0.5)
